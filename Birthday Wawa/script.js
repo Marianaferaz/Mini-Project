1562f1ad76594b8d0b7e9ac84cb052048f9c65ed
Maria Nathasya <nathasyamaria80@gmail.com>
2025-07-08 10:31:03 +0700

    push wawa bday

---
+++ Birthday Wawa/script.js
@@ -0,0 +1,20 @@
+const audio = document.querySelector('audio');
+  const body = document.body;
+
+  audio.addEventListener('play', () => {
+    body.classList.add('play-music');
+  });
+
+  audio.addEventListener('pause', () => {
+    body.classList.remove('play-music');
+  });
+
+  $(function(){
+
+  if (!$('.envelope').hasClass('open')){
+    $('.envelope').click(function(){
+      $(this).removeClass('new').addClass('open');
+    });
+  }
+  
+});
